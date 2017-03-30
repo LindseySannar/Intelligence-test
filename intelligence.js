882b816f797f9abf1e05dec3410c032f7b46112c
Unknown <lsannar1@mail.csuchico.edu>
2017-03-29 20:32:04 -0700

    added results page and strted cookies

--- intelligence.js
+++ intelligence.js
@@ -5,6 +5,7 @@ if(getAnswer1 == "reactive"){
     document.getElementById("question1").style.color = "green";
     document.getElementById("response1").innerHTML = "Correct!";
 
+
   } else {
     document.getElementById("question1").style.color = "red";
   }
@@ -62,6 +63,63 @@ if(getAnswer3=="Reviled"){
     document.getElementById("response4").innerHTML = "Correct!";
   }
     }
+    function validateAnswer5(){
+    var getAnswer5 = document.getElementById("question5").value;
+
+    if (getAnswer5=="revel"){
+      document.getElementById("question5").style.color = "green";
+      document.getElementById("response5").innerHTML = "Correct!";
+
+    }
+    else{
+      document.getElementById("question5").style.color = "red";
+
+    }
+    if(getAnswer4=="Revel"){
+      document.getElementById("question5").style.color = "green";
+      document.getElementById("response5").innerHTML = "Correct!";
+    }
+      }
+      function validateAnswer6(){
+      var getAnswer6 = document.getElementById("question6").value;
+
+      if (getAnswer6=="rotates"){
+        document.getElementById("question6").style.color = "green";
+        document.getElementById("response6").innerHTML = "Correct!";
+
+      }
+      else{
+        document.getElementById("question6").style.color = "red";
+
+      }
+      if(getAnswer4=="Rotates"){
+        document.getElementById("question6").style.color = "green";
+        document.getElementById("response6").innerHTML = "Correct!";
+      }
+function setCookie(cname, cvalue, exdays) {
+  var d = new Date();
+  d.setTime(d.getTime() + (exdays*24*60*60*1000));
+  var expires = "expires="+d.toUTCString();
+  document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+}
+function getCookie(cname) {
+  var name = cname + "=";
+  var ca = document.cookie.split(';');
+  for(var i = 0; i < ca.length; i++) {
+    var c = ca[i];
+    while (c.charAt(0) == ' ') {
+      c = c.substring(1);
+    }
+    if (c.indexOf(name) == 0) {
+      return c.substring(name.length, c.length);
+    }
+  }
+  return "";
+}
+
+function calculateScore(){
+  
+        }
 
 // create class for colors for the border of the answer
 //add or remove under the
