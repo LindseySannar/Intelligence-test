3f9e8897f87262c7706f72560e719c6b067ade36
Unknown <lsannar1@mail.csuchico.edu>
2017-03-30 20:05:00 -0700

    not really sure what im doing

--- intelligence.js
+++ intelligence.js
@@ -1,38 +1,32 @@
+//Lindsey Sannar
 function validateAnswer1(){
 var getAnswer1 = document.getElementById("question1").value;
 
-if(getAnswer1 == "reactive"){
+if(getAnswer1 == "reactive" || getAnswer1 == "Reactive"){
     document.getElementById("question1").style.color = "green";
     document.getElementById("response1").innerHTML = "Correct!";
-
 
   } else {
     document.getElementById("question1").style.color = "red";
   }
-  if(getAnswer1=="Reactive"){
-    document.getElementById("question1").style.color = "green";
-    document.getElementById("response1").innerHTML = "Correct!";
-  }
+
   }
   function validateAnswer2(){
   var getAnswer2 = document.getElementById("question2").value;
 
-    if(getAnswer2=="aligned"){
+    if(getAnswer2=="aligned" || getAnswer2=="Aligned"){
       document.getElementById("question2").style.color = "green";
       document.getElementById("response2").innerHTML = "Correct!";
     }
 else{
       document.getElementById("question2").style.color = "red";
     }
-if(getAnswer2=="Aligned"){
-      document.getElementById("question2").style.color = "green";
-      document.getElementById("response2").innerHTML = "Correct!";
-    }
+
   }
 function validateAnswer3(){
 var getAnswer3 = document.getElementById("question3").value;
 
-if (getAnswer3=="reviled"){
+if (getAnswer3=="reviled" || getAnswer3=="Reviled"){
   document.getElementById("question3").style.color = "green";
   document.getElementById("response3").innerHTML = "Correct!";
 
@@ -41,15 +35,11 @@ else{
   document.getElementById("question3").style.color = "red";
 
 }
-if(getAnswer3=="Reviled"){
-  document.getElementById("question3").style.color = "green";
-  document.getElementById("response3").innerHTML = "Correct!";
-}
   }
   function validateAnswer4(){
   var getAnswer4 = document.getElementById("question4").value;
 
-  if (getAnswer4=="diaper"){
+  if (getAnswer4=="diaper" || getAnswer4=="Diaper"){
     document.getElementById("question4").style.color = "green";
     document.getElementById("response4").innerHTML = "Correct!";
 
@@ -58,15 +48,11 @@ if(getAnswer3=="Reviled"){
     document.getElementById("question4").style.color = "red";
 
   }
-  if(getAnswer4=="Diaper"){
-    document.getElementById("question4").style.color = "green";
-    document.getElementById("response4").innerHTML = "Correct!";
-  }
     }
     function validateAnswer5(){
     var getAnswer5 = document.getElementById("question5").value;
 
-    if (getAnswer5=="revel"){
+    if (getAnswer5=="revel" || getAnswer5=="Revel"){
       document.getElementById("question5").style.color = "green";
       document.getElementById("response5").innerHTML = "Correct!";
 
@@ -75,15 +61,12 @@ if(getAnswer3=="Reviled"){
       document.getElementById("question5").style.color = "red";
 
     }
-    if(getAnswer4=="Revel"){
-      document.getElementById("question5").style.color = "green";
-      document.getElementById("response5").innerHTML = "Correct!";
-    }
+
       }
       function validateAnswer6(){
       var getAnswer6 = document.getElementById("question6").value;
 
-      if (getAnswer6=="rotates"){
+      if (getAnswer6=="rotates" || getAnswer6=="Rotates"){
         document.getElementById("question6").style.color = "green";
         document.getElementById("response6").innerHTML = "Correct!";
 
@@ -92,18 +75,31 @@ if(getAnswer3=="Reviled"){
         document.getElementById("question6").style.color = "red";
 
       }
-      if(getAnswer4=="Rotates"){
-        document.getElementById("question6").style.color = "green";
-        document.getElementById("response6").innerHTML = "Correct!";
-      }
     }
-function setCookie(cname, cvalue, exdays) {
+    function submitAnswer1(){
+      setCookie("correct", 2, 1);
+      window.location.href='intelligence2.html';
+    }
+    function submitAnswer2(){
+      var total = getCookie("correct");
+      getCookie("correct");
+      window.location.href='intelligence3.html';
+    }
+    function submitAnswer3(){
+      window.location.href='intelligence4.html';
+    }
+    function submitAnswer4(){
+      window.location.href='results.html';
+
+    }
+    //courtesty of w3schools from: http://www.w3schools.com/js/js_cookies.asp
+function setCookie(cname, cvalue, exdays){
 var d = new Date();
   d.setTime(d.getTime() + (exdays*24*60*60*1000));
  var expires = "expires="+d.toUTCString();
 document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
 }
-function getCookie(cname) {
+function getCookie(cname){
 var name = cname + "=";
   var ca = document.cookie.split(';');
   for(var i = 0; i < ca.length; i++) {
@@ -117,10 +113,11 @@ var name = cname + "=";
   }
   return "";
 }
-
 function calculateScore(){
-
 }
 
 // create class for colors for the border of the answer
-//add or remove under the
+// each function after first page should set a cookie and call the one from the last page.
+// at the end the function should call the cookies and add them up for the total.
+// var total should add the previous value to the current value
+// the function that calls the cookies should also have the link to the next page with the window.location.href=
